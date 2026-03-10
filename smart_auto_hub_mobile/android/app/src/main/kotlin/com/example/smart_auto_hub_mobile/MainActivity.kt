88bfc26f665eab2e7a6342c506ce31e8c2b98106
Kavindu <kavinduuni@gmail.com>
2026-03-10 19:59:53 +0530

    feat: Initialize new Flutter project with multi-platform support.

--- smart_auto_hub_mobile/android/app/src/main/kotlin/com/example/smart_auto_hub_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.smart_auto_hub_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- smart_auto_hub_mobile/smart_auto_hub_app/android/app/src/main/kotlin/com/example/smart_auto_hub_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.smart_auto_hub_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
